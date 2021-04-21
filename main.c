dc23d67de5d544dceb0738cd1062221e42f1a739
gunter <justmegunter@protonmail.com>
2021-04-21 18:21:05 +0200

    fixes 0 itoa_base

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acrucesp <acrucesp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 16:58:21 by acrucesp          #+#    #+#             */
-/*   Updated: 2021/04/21 17:33:22 by acrucesp         ###   ########.fr       */
+/*   Updated: 2021/04/21 18:17:42 by acrucesp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ int        main(int argc, char *argv[])
 		static char *s_hidden = "hi \0 lolololo \0";
     	printf("******************\n");
 		printf("Test\n");
-    	n = printf("%d\n", -267);
+    	n = printf("%p\n", NULL);
     	printf("!!! %i !!!\n", n);
-    	n = ft_printf("%d\n", -267);
+    	n = ft_printf("%p\n", NULL);
     	printf("!!! %i !!!\n", n);
 	}
 	if (*argv[1] == '%')
--- src/ft_itoa_base.c
+++ src/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: acrucesp <acrucesp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 20:49:12 by acrucesp          #+#    #+#             */
-/*   Updated: 2021/04/21 17:27:31 by acrucesp         ###   ########.fr       */
+/*   Updated: 2021/04/21 18:14:11 by acrucesp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,16 @@ char		*ft_itoa_base(long long unsigned int n,  int base, char *elm_base)
 	}
 	else
 		nn = n;
+	if (nn == 0)
+		nn = -1;
 	ret = malloc(sizeof(char) * i + 1);
 	*(ret + i)  = '\0';
-	while (nn != 0)
+	while (nn > 0)
 	{
 		ret[--i] = elm_base[nn % base];
 		nn = nn / base;
 	}
-	if (nn == 0)
+	if (nn == -1)
 		*ret = '0';
 	return(ret);
 }
