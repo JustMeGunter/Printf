f85c0fd040e416567f4c9ffe78f6cd9b52802689
gunter <justmegunter@protonmail.com>
2021-04-23 16:22:33 +0200

    temp fix zeroes

--- src/h_any_n.c
+++ src/h_any_n.c
@@ -6,7 +6,7 @@
 /*   By: acrucesp <acrucesp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 19:41:03 by acrucesp          #+#    #+#             */
-/*   Updated: 2021/04/22 20:33:42 by acrucesp         ###   ########.fr       */
+/*   Updated: 2021/04/23 16:20:47 by acrucesp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void			h_any_n(t_spf *esp, va_list *argp, char c)
 	else
 		nn = is_pnt_or_h(esp, va_arg(*argp, size_t), c);
 	esp->sublen = ft_strlen(nn);
+	if (esp->sublen == 1 && *nn == '0' && esp->h_p && esp->width == 0)
+		*nn = '\0';
+	else if (esp->sublen == 1 && *nn == '0' && esp->h_p)
+		*nn = ' ';
 	if (is_negative(esp, &nn) && !esp->h_p && esp->zero && esp->negative--)
 		esp->count += write(1, "-", 1);
 	if (!esp->left)
@@ -41,8 +45,11 @@ void			h_any_n(t_spf *esp, va_list *argp, char c)
 		draw_precision(esp);
 	if (c == 'p')
 		esp->count += write(1, "0x", 2);
-	while (nn[++i])
-		esp->count += write(1, &nn[i], 1);
+	if (esp->sublen > 0 && *nn != '0')
+		while (nn[++i])
+			esp->count += write(1, &nn[i], 1);
+	else
+		esp->count += write(1, " ", 1);
 	if (esp->left)
 		while (esp->width-- > esp->sublen && esp->width > esp->precision)
 			draw_width(esp);
