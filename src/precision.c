e7fac50c82df4b7c97629de59079f3488e27e6cb
gunter <justmegunter@protonmail.com>
2021-04-24 16:58:24 +0200

    fix precision arch linux

--- src/precision.c
+++ src/precision.c
@@ -6,7 +6,7 @@
 /*   By: acrucesp <acrucesp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 18:01:30 by acrucesp          #+#    #+#             */
-/*   Updated: 2021/04/22 20:10:09 by acrucesp         ###   ########.fr       */
+/*   Updated: 2021/04/24 16:18:32 by acrucesp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void			precision(t_spf *esp, va_list *argp)
 				while (*esp->content >= '0' && *esp->content <= '9')
 					esp->content++;
 		}
-		if (esp->precision == 0)
+		if (esp->precision == 0 && !esp->n_p)
 			esp->zero = 0;
 	}
 }
